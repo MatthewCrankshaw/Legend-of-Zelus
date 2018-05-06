9a0783695a7f7754aa852f44d013fd2643aa1975
Matthew C <mhcrankshaw2@gmail.com>
2018-05-07 09:00:11 +1200

    Changes to how enemy sprites look

--- src/game/graphics/sprite/mob_sprites/PlayerSprite.java
+++ src/game/graphics/sprite/mob_sprites/PlayerSprite.java
@@ -10,7 +10,9 @@ import game.levels.tile.Tile;
  */
 public class PlayerSprite extends AnimatedSprite {
 
-    public static Sprite[][] playerSprites = loadPlayer();
+    public static Sprite[][] playerSprites = loadPlayer(0);
+    public static Sprite[][] zombieSprites = loadPlayer(1);
+    public static Sprite[][] enemySprites = loadPlayer(2);
     public static Sprite[] playerAttackSprites = loadAnimatedSprite(Tile.TILE_SIZE*2, new int[]{0,1,2}, new int[]{0,0,0}, SpriteSheet.characterAnimations16);
 
     public static Sprite[] swimming = loadAnimatedSprite(16, 0, 0, 3, SpriteSheet.statusEffects_16);
@@ -19,15 +21,12 @@ public class PlayerSprite extends AnimatedSprite {
         super(16, x, y, SpriteSheet.characterMovement_16);
     }
 
-    public static Sprite[][] loadPlayer(){
-        //player forward    = currentSprite[3][0..3]
-        //player right      = currentSprite[2][0..3]
-        //player left       = currentSprite[1][0..3]
-        //player backward   = currentSprite[0][0..3]
+    public static Sprite[][] loadPlayer(int spriteNum){
+        //spriteNum is the number that the sprite is on the y axis of the sprite page
 
         Sprite[][] sprites = new Sprite[4][4];
         for (int anim = 0; anim < 4; anim++) {
-            sprites[anim] = loadAnimatedSprite(Tile.TILE_SIZE*2, anim * 4, 0, 4, SpriteSheet.characterMovement_16);
+            sprites[anim] = loadAnimatedSprite(Tile.TILE_SIZE*2, anim * 4, spriteNum, 4, SpriteSheet.characterMovement_16);
         }
         return sprites;
     }
